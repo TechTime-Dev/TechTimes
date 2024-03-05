1f1e86c09d43207bc963b825361b4fb455798c13
Eliza612 <yameng612@gmail.com>
2024-03-04 19:26:29 -0500

    add nav bar and tailwind configuration

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="font-semibold py-3 bg-[#e6f2f7] flex items-center justify-between mx-auto">
+      <div className="navbar-div flex items-center justify-between w-full pr-20 pl-20">
+        <div className="brand flex items-center">
+          <img
+            src={require("../../public/logo-removebg.png").default}
+            width="150"
+            height="150"
+          ></img>
+          <Link to="/" className="text-black text-6xl font-josefin">
+            Tech Times
+          </Link>
+        </div>
+        <div classname="nav-div flex justify-between items-center">
+          <Link
+            to="/"
+            className="text-black text-3xl hover:text-gray-300 font-ptsans ml-6"
+          >
+            News Feed
+          </Link>
+          <Link
+            to="/favorite"
+            className="text-black text-3xl hover:text-gray-300 font-ptsans ml-6"
+          >
+            Favorites
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
