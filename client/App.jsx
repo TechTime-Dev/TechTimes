bb9cd10ac4b6bbe04bc1b4d2a3f63032ac7e39a9
Eliza612 <yameng612@gmail.com>
2024-03-05 17:42:07 -0500

    remove article cards for now

--- client/App.jsx
+++ client/App.jsx
@@ -12,7 +12,6 @@ const Layout = () => {
   return (
     <>
       <NavBar />
-      <ArticleContainer/>
       <Outlet />
     </>
   );
--- client/components/articleCard.js
+++ client/components/articleCard.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import FavoriteBoarderIcon from '@mui/icons-material/FavoriteBorder.js';
-import FavoriteIcon from '@mui/icons-material/Favorite.js';
+import React, { useState } from "react";
+// import FavoriteBoarderIcon from '@mui/icons-material/FavoriteBorder.js';
+// import FavoriteIcon from '@mui/icons-material/Favorite.js';
 
 export default function ArticleCard(props) {
-  console.log('Image prop:', props.img); // Log the img prop
+  console.log("Image prop:", props.img); // Log the img prop
 
   const [isfavorite, setIsFavorite] = useState(false);
   const [linkClicked, setLinkClicked] = useState(false);
@@ -14,10 +14,10 @@ export default function ArticleCard(props) {
       //update favorite icon
       setIsFavorite((prev) => !prev);
       //send to favorites List
-      const response = await fetch('/favorite', {
-        method: 'POST',
+      const response = await fetch("/favorite", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           articleId: props.articleId,
@@ -26,17 +26,17 @@ export default function ArticleCard(props) {
         }),
       });
     } catch (error) {
-      console.log('Error', error);
+      console.log("Error", error);
     }
   };
 
   const handleHistory = async () => {
     //send to history list
     try {
-      const response = await fetch('/history', {
-        method: 'POST',
+      const response = await fetch("/history", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           articleId: props.articleId,
@@ -45,27 +45,27 @@ export default function ArticleCard(props) {
       });
       setLinkClicked(true);
     } catch (error) {
-      console.log('Error', error);
+      console.log("Error", error);
     }
   };
 
   return (
-    <div className='article-card-container'>
-      <div className='right'>
+    <div className="article-card-container">
+      <div className="right">
         {/* <p className='article-image'>Image</p> */}
-        <img src={props.img} className='article-image' />
-        {isfavorite ? (
+        <img src={props.img} className="article-image" />
+        {/* {isfavorite ? (
           <FavoriteIcon className='favorite-icon' onClick={handleFavorite} />
         ) : (
           <FavoriteBoarderIcon
             className='favorite-icon'
             onClick={handleFavorite}
           />
-        )}
+        )} */}
       </div>
-      <div className='article-card'>
+      <div className="article-card">
         <p
-          className={linkClicked ? 'link clicked' : 'link'}
+          className={linkClicked ? "link clicked" : "link"}
           onClick={handleHistory}
         >
           Title: {props.title}
