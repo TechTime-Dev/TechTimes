f02f345babefe48983378b9712ac62e1461a81b8
mfronheiser <meredith.fronheiser@gmail.com>
2024-03-05 19:03:40 -0500

    update

--- client/components/articleContainer.js
+++ client/components/articleContainer.js
@@ -3,25 +3,32 @@ import ArticleCard from './articleCard.js';
 import mockArticles from './mockData.js'; // Import mock data
 
 export default function ArticleContainer() {
-    const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
 
-    useEffect(() => {
-      fetch('/getNews')
-        .then((response) => response.json())
-        .then((data) => {
-          setArticles(data.articles);
-        })
-        .catch((error) => {
-          console.error('Error fetching articles:', error);
-        });
-    }, []);
+  useEffect(() => {
+    fetch('/getNews')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setArticles(data.articles);
+      })
+      .catch((error) => {
+        console.error('Error fetching articles:', error);
+      });
+  }, []);
+
+  console.log(articles);
 
   return (
     <div className='article-container'>
       {articles.map((article) => (
         <ArticleCard
           key={article.id}
-          title={<a href={article.link} target="_blank" rel="noopener noreferrer">{article.title}</a>}
+          title={
+            <a href={article.link} target='_blank' rel='noopener noreferrer'>
+              {article.title}
+            </a>
+          }
           author={article.author}
           source={article.source}
           date={article.date}
