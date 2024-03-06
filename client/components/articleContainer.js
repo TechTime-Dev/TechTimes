27c3192bed1afac81c93ad02dd0b52d20bb78b7c
Eliza612 <yameng612@gmail.com>
2024-03-05 21:47:49 -0500

    add login and style update

--- client/components/articleContainer.js
+++ client/components/articleContainer.js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import ArticleCard from './articleCard.js';
-import mockArticles from './mockData.js'; // Import mock data
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import ArticleCard from "./articleCard.js";
+import mockArticles from "./mockData.js"; // Import mock data
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { updateSearchArticles } from "../redux/articleSlice.js";
 
 export default function ArticleContainer() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   // const [articles, setArticles] = useState([]);
   const articlesArray = useSelector((state) => state.articles.searchArticles);
 
   useEffect(() => {
-    fetch('http://localhost:3000/getNews')
+    fetch("http://localhost:3000/getNews")
       .then((response) => response.json())
       .then((data) => {
-        console.log('line 12', data);
-        console.log('articlesArray', data.articles);
-        dispatch(updateSearchArticles(data.articles))
-        console.log('after dispatching', articlesArray);
+        console.log("line 12", data);
+        console.log("articlesArray", data.articles);
+        dispatch(updateSearchArticles(data.articles));
+        console.log("after dispatching", articlesArray);
       })
       .catch((error) => {
-        console.error('Error fetching articles:', error);
+        console.error("Error fetching articles:", error);
       });
   }, []);
 
   //   console.log(articles);
 
   return (
-    <div className='article-container'>
+    <div className="article-container max-w-[1600px] mx-auto justify-center items-center h-screen pl-8">
       {articlesArray.map((article, index) => (
         <ArticleCard
           key={index}
           title={
-            <a href={article.url} target='_blank' rel='noopener noreferrer'>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
               {article.title}
             </a>
           }
